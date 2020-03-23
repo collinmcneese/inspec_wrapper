4edba594ffe9c94fb641fa953c68b2da99df9a93
Collin McNeese <cmcneese@chef.io>
2020-03-23 09:38:01 -0500

    v1.0.2
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- controls/profile1.rb
+++ controls/profile1.rb
@@ -1,18 +1,15 @@
 # copyright: 2018, The Authors
 
 title 'profile1'
-case os['family']
-when 'redhat'
-  describe file('/tmp') do
+control 'profile1_control_1' do                        # A unique ID for this control
+  impact 0.7                                # The criticality, if this control fails.
+  title 'Create /tmp directory'             # A human-readable title
+  desc 'An optional description...'
+
+  only_if { os.redhat? }
+
+  describe file('/tmp') do                  # The actual test
     it { should be_directory }
   end
-
-  control 'tmp-1.0' do                        # A unique ID for this control
-    impact 0.7                                # The criticality, if this control fails.
-    title 'Create /tmp directory'             # A human-readable title
-    desc 'An optional description...'
-    describe file('/tmp') do                  # The actual test
-      it { should be_directory }
-    end
-  end
 end
+
