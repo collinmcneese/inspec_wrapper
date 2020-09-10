a5595a9da9c5228832aa0c6e1cb67d264347d50a
Collin McNeese <cmcneese@chef.io>
2020-09-10 16:48:26 -0500

    inspec 4.22.8
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -4,4 +4,4 @@ $pkg_version="$(Get-Content $PLAN_CONTEXT\..\VERSION)"
 $pkg_maintainer="Collin McNeese <cmcneese@chef.io>"
 $pkg_license=@("Apache-2.0")
 $pkg_description="Effortless Inspec Audit"
-$pkg_scaffolding="chef/scaffolding-chef-inspec"
+$pkg_scaffolding="chef/scaffolding-chef-inspec/4.22.8"
--- habitat/plan.sh
+++ habitat/plan.sh
@@ -4,4 +4,4 @@ pkg_version=$(cat "${PLAN_CONTEXT}/../VERSION")
 pkg_maintainer="Collin McNeese <cmcneese@chef.io>"
 pkg_license=("Apache-2.0")
 pkg_description="Effortless Inspec Audit"
-pkg_scaffolding="chef/scaffolding-chef-inspec"
+pkg_scaffolding="chef/scaffolding-chef-inspec/4.22.8"
